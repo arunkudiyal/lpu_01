2b429fe3caee415e7b5ae76a16ba37f39a1d04c6
arunkudiyal <arunkudiyal@icloud.com>
2025-02-06 14:02:50 +0530

    Prims Algorithm

--- src/PrimsMST.java
+++ src/PrimsMST.java
@@ -9,9 +9,41 @@ public class PrimsMST {
     public void prims(int start) {
         boolean[] visited = new boolean[V];
         int[] cost = new int[V];
-        Arrays.fill(cost, Integer.MAX_VALUE);
+        int[] parent = new int[V];
+        parent[start] = -1;
+        // Arrays.fill(cost, Integer.MAX_VALUE);
+        for(int i=0; i < V; i++) cost[i] = Integer.MAX_VALUE;
         cost[start] = 0;
-        
+        for(int i=0; i < V-1; i++) {
+            int u = minVertex(cost, visited);               // u --> Source
+            visited[u] = true;
+            for(int v=0; v < V; v++) {                      // v --> Destination
+                if(graph[u][v] > 0 && !visited[v]
+                        && cost[v] >= graph[u][v]) {
+                    cost[v] = graph[u][v];
+                    parent[v] = u;
+                }
+            }
+        }
+        // printing cost[]
+        int totalMinCost = 0;
+        System.out.println("S_to_D\tCost");
+        for(int i=0; i < V; i++) {
+            totalMinCost += cost[i];
+            System.out.println(parent[i] + " - " + i + "\t" +cost[i] + " ");
+        }
+        System.out.println();
+        System.out.println(totalMinCost);                       // 37
+    }
+    public int minVertex(int[] cost, boolean[] visited) {
+        int minVertex = -1; int minCost = Integer.MAX_VALUE;
+        for(int i=0; i < V; i++) {
+            if(cost[i] < minCost && !visited[i]) {
+                minCost = cost[i];
+                minVertex = i;
+            }
+        }
+        return minVertex;
     }
     public static void main(String[] args) {
         PrimsMST mst = new PrimsMST(9);
