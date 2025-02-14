9be501a6792a8df359becf54beea0b2cbd477605
arunkudiyal <arunkudiyal@icloud.com>
2025-02-14 16:13:36 +0530

    NQueens Problem

--- src/DynamicProgramming.java
+++ src/DynamicProgramming.java
@@ -2,6 +2,7 @@
 // technique --> Code Optimisation --> Reducing running time
 // 1. Memoization (storing intermediate results)
 //      Pre --> recursive code to problem
+import java.util.ArrayList;
 import java.util.Arrays;
 public class DynamicProgramming {
     public static long fibRecursive(int N) {
@@ -9,15 +10,21 @@ public class DynamicProgramming {
         if(N == 1) return 1;
         return fibRecursive(N - 1) + fibRecursive(N -2);
     }
+    public static int gridTravellerRecursive(int N, int M) {
+        if(N == 0 || M == 0) return 0;
+        if(N == 1 && M == 1) return 1;
+        return gridTravellerRecursive(N, M-1) +
+                gridTravellerRecursive(N-1, M);
+    }
     public static long fibDP(int N) {
         // step1 | create a memo (storage)
         //      --> 1. type of the solution - Number
         //      --> 2. what data structure to use - Array
         //      --> 3. size of the memo (N=5, 0...4 f(0), f(1), f(2), f(3), f(4) )
+        //      --> 4. initial value of the memo [how would you store that result is not found]
         long[] memo = new long[N+1];
-        // step2 | initial value of the memo [how would you store that result is not found]
         Arrays.fill(memo, -1);
-        // step3 | filling the memo with already known results
+        // step2 | filling the memo with already known results
         memo[0] = 0;
         memo[1] = 1;
         // step4 | call a utility function & pass all the parameters along with the memo
@@ -34,8 +41,30 @@ public class DynamicProgramming {
         memo[N] = fibDPUtil(N-1, memo) + fibDPUtil(N-2, memo);
         return memo[N];
     }
+    public long fibTabular(int N) {
+        long[] dp = new long[N+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i <= N; i++) dp[i] = dp[i-1] + dp[i-2];
+        return dp[N];
+    }
+    public static int gridTravellerTabular(int N, int M) {
+        int[][] dp = new int[N+1][M+1];
+        for(int i=0 ; i <= M; i++) dp[0][i] = 0;
+        for(int i=0 ; i <= N; i++) dp[i][0] = 0;
+        dp[1][1] = 1;
+        for(int i=0 ; i <= M; i++) dp[1][i] = 1;
+        for(int i=0 ; i <= N; i++) dp[i][1] = 1;
+        for(int i = 2; i <= N; i++) {
+            for(int j = 2; j <= M; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[N][M];
+    }
     public static void main(String[] args) {
-        System.out.println( fibDP(100) );                     // 2586269025
-        System.out.println( fibRecursive(100) );              // 2586269025
+        System.out.println( fibDP(100) );                           // 2586269025
+        // System.out.println( fibRecursive(100) );                    // 2586269025
+        System.out.println( gridTravellerTabular(25, 25) );         // 3
     }
 }
---
+++ src/NQueens.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+class NQueens {
+    public void nQueens(List<List<String>> answer, char[][] board, int row, int n) {
+        if(row == n) {
+            List<String> solution = generateAnswer(board, n);
+            answer.add(solution);
+            return;
+        }
+        for(int col=0; col < n; col++) {
+            if( isSafe(row, col, board, n) ) {
+                board[row][col] = 'Q';
+                nQueens(answer, board, row + 1, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+    public boolean isSafe(int row, int col, char[][] board, int n ) {
+        // Horizonal & Vertical
+        for(int i=0; i < n; i++) {
+            if(board[i][col] == 'Q') return false;
+            if(board[row][i] == 'Q') return false;
+        }
+        // Upper-left diagonal
+        for(int i=row, j=col; i >= 0 && j >= 0; i--, j--) {
+            if(board[i][j] == 'Q') return false;
+        }
+        // Upper-right diagonal
+        for(int i=row, j=col; i >= 0 && j < n; i--, j++) {
+            if(board[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+    public List<String> generateAnswer(char[][] board, int n) {
+        List<String> solution = new ArrayList<>();
+        for(int row=0; row < n; row++) {
+            String str = "";
+            for(int col=0; col < n; col++) {
+                str += board[row][col];
+            }
+            solution.add(str);
+        }
+        return solution;
+    }
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> answer = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for(int row=0; row <n; row++) {
+            for(int col=0; col < n; col++) {
+                board[row][col] = '.';
+            }
+        }
+        // updates the answer from the board
+        nQueens(answer, board, 0, n);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
